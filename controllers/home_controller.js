e2324128b9cf4eb8e3df0fc640b4f970fceb82a9
niju.prem <nijuprem7@gmail.com>
2023-02-21 16:34:02 +0530

    Displayed posts and the user who posted it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,25 @@
+const Post = require('../models/post')
+
 module.exports.home = function(req, res){
     // console.log(req.cookies);
-    res.cookie('user_id', 100);
-    res.cookie('name', 'random');
-    return res.render('home', {
-        title: "Home"
+    // res.cookie('user_id', 100);
+    // res.cookie('name', 'random');
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Home",
+    //         posts : posts
+    //     });
+    // })
+
+    // Populate user of each obj
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "MaceBook | Home",
+            posts : posts
+        });
     });
+
 }
 
 // module.exports.actionName = function(req, res){}
