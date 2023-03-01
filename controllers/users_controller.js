8c155f4451ea3328815aca3399b51208de5167e7
niju.prem <nijuprem7@gmail.com>
2023-03-01 11:02:23 +0530

    Optimzed the code by adding partials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,26 +1,23 @@
 const User = require('../models/user');
 
-// module.exports.profile = function(req, res){
-
-//     if(req.cookies.user_id){
-//         User.findById(req.cookies.user_id, function(err, user){
-//             if(user){
-//                 return res.render('user_profile', {
-//                     title: 'User Profile',
-//                     user: user
-//                 })
-//             }
-//             // else {return res.redirect('/users/sign-in');} 
-//         })
-//     }
-//     else {return res.redirect('/users/sign-in');}
-    
-// }
-
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
+    User.findById(req.params.id, function(err, user){
+        return res.render('user_profile', {
+            title: 'User Profile',
+            profile_user: user
+        });
     })
+    
+}
+
+module.exports.update = function(req, res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
 }
 
 // Render Sign Up page
