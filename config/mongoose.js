45ee8ae3cb4a2622d8c23cca1a16db2157e3ca29
niju.prem <nijuprem7@gmail.com>
2023-03-18 12:31:26 +0530

    Added MongoDB Atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 const env = require('./enviornment');
 
-mongoose.connect(`mongodb://127.0.0.1:27017/${env.db}`);
-
+mongoose.connect(`mongodb+srv://nijuprem7:nptestdb@cluster0.x2suycd.mongodb.net/?retryWrites=true&w=majority`);
 const db = mongoose.connection;
 
 main().catch(err => console.log(err));
-
+ 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/macebook_development');
-  
+  await mongoose.connect("mongodb+srv://nijuprem7:nptestdb@cluster0.x2suycd.mongodb.net/?retryWrites=true&w=majority");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
